6d12ebedfc6da1bb777a67492e290b104ace0478
Mario Aguilar <mal90developer@gmail.com>
2022-04-29 18:38:40 +0200

    feature: navbar

---
+++ src/app/modules/movies/movies-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { MoviesScreenComponent } from './movies-screen/movies-screen.component';
+import { NewMovieComponent } from './new-movie/new-movie.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MoviesScreenComponent,
+  },
+  {
+    path: '/detail/:id',
+    component: MovieDetailComponent
+  },
+  {
+    path: 'new-movie',
+    component: NewMovieComponent
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class MoviesRoutingModule { }
---
+++ src/app/shared/modules/material/material.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+const materialModules = [
+  MatToolbarModule,
+  MatSidenavModule,
+  MatButtonModule,
+  FlexLayoutModule,
+  MatIconModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ...materialModules
+  ],
+  exports: [
+    ...materialModules
+  ]
+})
+export class MaterialModule { }
+
